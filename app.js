28a65e539556413482bcfbb646c8881f19c5f4d4
Haider-Ali-Jaffri <hdali020@gmaial.com>
2020-06-15 23:29:22 +0500

    J.S Assignment-01

---
+++ app.js
@@ -0,0 +1,270 @@
+// alert ("Welcome to My Page")
+// alert ("Happy codding !")
+
+// var a = 50;
+// var b = 50;
+// var c = a+b;
+// alert(c)
+
+// var a = 50;
+// var b = 50;
+// var c = a+b;
+// console.log (c);
+
+// var y = "Haider";
+// var x = "Ali";
+// var z = y+x;
+// alert(z)
+
+// document.write ("I am beginer to JS, <br> Welcome to my page")
+
+// alert ("Chapter 2");
+
+
+// var a = "Haider Ali";
+// alert (a)
+// var age = "15 years old";
+// alert (age)
+// alert ("Certified Mobile Development");
+
+// var email = "hdali020@gamil.com"
+// alert (email)
+
+// var book = ("A smarter way to learn JavaScript")
+// alert(book)
+// document.write ("Yah! I can write HTML content through JavaScript")
+
+// var design = ("----------&$*$&----------")
+// alert(design)
+
+
+// alert ("Chapter 03");
+
+// var age = ("I am 24 years old")
+// alert(age)
+
+// document.write ("My birth year is 1996")
+// document.write ("Ali ordered 10 T-shirt(s) on XYZ Clothing sotre")
+
+
+// alert ("CHAPTER 04")
+
+
+// document.write ("<h1> Rules for naming JS variable </h1><br> A variable name can contain only letters, numbers, dollar signs, and underscores. <br> Variable names are case sensitive. <br> Variable names should not be JS keywords. <br> Variable must begin with a names any keywords, dollar sign, or underscores.")
+
+
+// alert ("========CHAPTER 05")
+
+
+// var a = (3)
+// var b = (5)
+// var c = (a+b)
+// document.write("Sum of 3 and 5 is" + " " + c)
+
+// alert("for subtraction")
+// var a = (3)
+// var b = (5)
+// var c = (a-b)
+// document.write("Sub of 3 and 5 is" + " " + c)
+
+// alert("for Multiply")
+// var a = (3)
+// var b = (5)
+// var c = (a*b)
+// document.write("Mul of 3 and 5 is" + " " + c)
+
+// alert("for Divide")
+// var a = (3)
+// var b = (5)
+// var c = (a/b)
+// document.write("Division of 3 and 5 is" + " " + c)
+
+// alert("for Modulus")
+// var a = (3)
+// var b = (5)
+// var c = (a%b)
+// document.write("Modulus of 3 and 5 is" + " " + c)
+
+
+// var cost1 = (600)
+// var costof5 = (5)
+// var tcost = (cost1*costof5)
+// document.write("Total cost to buy 5 tickets to a movie is" + " " + tcost)
+
+// alert("Chapter 05 Task 5=================")
+// for(var i =1; i <=10; i++){
+//     document.write("5" + "x" + i + "=" + 5*i + "<br>")
+// }
+
+
+// var celsi = (37);
+// var celsius = (C*9)/ 5 + 32;
+// console.log(Celsi + "C is" + celsius + "F");
+// var farhenit = (20);
+// var fahrenheit =((farhenit - 32)*5)/9;
+// console.log(farhenit + "F is" + fahrenheit + "C");
+
+// var price1 =(200)
+// var quantity1 = (5)
+// var price2 = (100)
+// var quantity2 = (4)
+// var shipcharge = (500)
+// var totalCost = (price1 * quantity1 + price2 * quantity2 + shipcharge)
+// document.write("<b> Price of item 1 is 200 <br> Quantity of item 1 is 5 <br> Price of item 2 is 100 <br> Quantity of item 2 is 4 <br> Shipping charges is 500 <br> Total Cost of your order is </b>" + " " + totalCost )
+
+
+
+// var marks = (1100);
+// var obtain = (555);
+// var percen = ((obtain/marks) *100);
+// document.write("Total Marks: 1100 <br> Marks obtained 550 <br> Percentage:" + " " +percen)
+
+
+// var us = (10)
+// var sr = (25)
+// var exhUS = (104.80)
+// var exhSR = (28)
+// var currencyPK = ( us*exhUS + sr*exhSR)
+// document.write("Currency in PKR" + " " +currencyPK)
+
+
+// var dmsrule = ((5*10)/2);
+// document.write("Answer is" + " " +dmsrule)
+
+
+// alert(===Task 11===)
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var age = (birthYear  birthYear);
+// document.write("jdsf" + " " +age  )
+
+
+// var radius = (20);
+// var circumference = (Math.PI * 2*radius)
+// var area = (Math.PI * radius*radius);
+// document.write("<b> Radius of Circule:20 <br>  The circumference: </b>" +circumference, "<br> <b> The Area is: </b>" +area)
+
+
+// var age = (15)
+// var maxAge = (65)
+// var perDay = (3)
+// var total = (perDay * 365) * (maxAge - age)
+// document.write("<b> Favorite Snacks: Chocolate chip <br> Curent age:15 <br> Estimated Max age:65 <br> Amount of snacks per day:3 <br> You will need </b>" +total+ "<b> at the age of </b>" +maxAge)
+
+
+// alert("===CHAPTER 6 TO 9===")
+
+// var a = (10);
+// var b = (++a + a++ + --a + a--);
+// document.write(+b)
+
+
+// var a =(2)
+// var b =(1)
+// var c = (--a - --b + ++b + b--)
+// document.write("Result is:" +c)
+
+// var a = (2)
+// var c = (--a)
+// document.write(+c)
+
+// var a = (2)
+// var b = (1)
+// var c = (--a - --b)
+// document.write(+c)
+
+// var a = (2)
+// var b = (1)
+// var c = (--a - --b + ++b)
+// document.write(+c)
+
+// var a = (2)
+// var b = (1)
+// var c = (--a - --b + ++b + b--)
+// document.write(+c)
+
+
+
+// var sub1 = ("math")
+// var sub2 = ("urdu")
+// var sub3 = ("english")
+// var total = (100);
+// var firstsub = (54);
+// var sectsub = (54);
+// var thirdsub = (48);
+// var atotal = (total+total+total)
+// var aobtained = (firstsub+sectsub+thirdsub)
+// var percentage = ((aobtained/atotal)*100)
+// document.write("Total marks:" +atotal, "<br> total obtained marks:" +aobtained, "<br> Percentage is" +percentage )
+
+
+
+
+// alert("$$ CHAPTER 9 TO 11 $$")
+
+
+
+// var city = prompt("Enter city")
+// if(city == "Karachi"){
+//     alert("welcome to city of lights")
+// }
+// else{
+//     alert("You are not allowed")
+// }
+
+// var gender = prompt("Enter your Gender")
+// if(gender == "male"){
+//     alert("Good Morning Sir")
+// }
+// else if(gender == "female"){
+//     alert("Good Morning Maam")
+// }
+// else{
+//     alert("Not found")
+// }
+
+
+// var a = 4;
+// if(++a === 5){
+//     alert("given condition for variable a is true")
+// }
+
+// var b = 82;
+// if(b++ === 83){
+//     alert("given condition for variable b is true")
+// }
+// document.write("condition is not displayed")
+ 
+
+// var c = 12;
+// if(c++ === 13){
+//     alert("condition 1 is true")
+// }
+// if(c === 13){
+//     alert("condition 2 is true")
+// }
+// if(++c < 14){
+//     alert("condition 3 is true")
+// }
+// if(c === 14){
+// alert("condition 4 is true")
+// }
+
+// document.write("condition 1 and 4 is false")
+
+
+// var materialCost = 20000; 
+// var laborCost = 2000;
+//  var totalCost = materialCost + laborCost;
+//   if (totalCost === laborCost + materialCost){
+//        alert("The cost equals"); 
+// }
+//  if (true){
+//     alert("True");
+// }
+// if (false){
+//      alert("False");
+// }if("car" < "cat"){
+//     alert("car is smaller than cat")
+// }
+
